4a1b19b5e411d77cb07cb546ef2267fe9de25d26
Saif CNIT <jubayerhossain111220@gmail.com>
2025-03-13 15:05:33 +0600

    updated favicon 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,8 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" sizes="any">
-      <link rel="icon" href="/abc-logo-icon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon.ico" sizes="any"/>
+        <link rel="icon" href="/abc-logo-icon.svg" type="image/svg+xml"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet"/>
