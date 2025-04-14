4057d712a9c023c6bc8e92b6e822b2e7f7fb2e30
Saif CNIT <jubayerhossain111220@gmail.com>
2025-04-14 16:21:49 +0600

    added reports page in dashboard

---
+++ src/app/dashboard/reports/page.js
@@ -0,0 +1,12 @@
+import Reports from '@/Pages/DashboardPages/Reports/Reports';
+import React from 'react';
+
+const ReportsPage = () => {
+    return (
+        <>
+            <Reports/>
+        </>
+    );
+};
+
+export default ReportsPage;
